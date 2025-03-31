686fa82558230eac61cb65af7d02b77224f3ee5f
Swatts96 <w0292233@nscc.ca>
2025-03-31 13:19:05 -0300

    added swagger for ui

--- TicketHub/Program.cs
+++ TicketHub/Program.cs
@@ -9,17 +9,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketHub API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
